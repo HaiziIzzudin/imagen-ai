2762ec1c4e643ec1d562d244431c45018266e690
haizi <thehub@iziizz.slmail.me>
2024-09-11 23:44:44 +0800

    2024-09-11 23:44:44

--- src/main.ts
+++ src/main.ts
@@ -56,7 +56,7 @@ function postRequest(prompt:string) {
       headers: myHeaders,
       body: raw,
       redirect: "follow" as RequestRedirect,
-      timeout: 150000 // 2.5 minutes
+      timeout: 180000 // 3 minutes
     };
     const apiBuffer = Buffer.from(API, 'base64');
     const apiDecoded = apiBuffer.toString('utf8');
